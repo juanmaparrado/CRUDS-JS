e45a7188a033a4d1f83d9c5d374c28e8bf7c3ab3
juanmaparrado <juanmanuel.parrado.fp@iescampanillas.com>
2023-01-30 20:44:13 +0100

    Infinite loop with mostrar()

--- Cookies/js/script.js
+++ Cookies/js/script.js
@@ -1,6 +1,6 @@
 "use strict"
 
-function guard() {
+function guardar() {
     const inputClave = document.getElementById("nombre");
     const inputValor = document.getElementById("apellido");
     saveCookie(inputClave.value,inputValor.value);
@@ -17,23 +17,4 @@ function saveCookie(clave,valor) {
     console.log(document.cookie);
 }
 
-var cookiePair = [];
-function getCookie(name) {
-    var cookieArr = document.cookie.split(";");
-        for(var i = 0; i < cookieArr.length; i++) {
-            cookiePair = cookieArr[i].split("=");
-           mostrar() 
-        }
 
-}
-function mostrar() {
-    for (let i = 0; i < cookiePair.length; i+2) {
-        let div = document.createElement('div');
-        if ((i%2) == 0) {
-            div.className = 'row mt-4'
-        }
-        div.className = 'col-md';
-        div.innerHTML = '<p>'+cookiePair[i]+'</p>';
-        document.body.appendChild(div);
-    }
-}
